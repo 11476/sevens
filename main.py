5c0adb35ea964e5c08ed72915645fc7dbe7fab1d
11476 <gjw3503@gmail.com>
2025-07-25 08:17:32 -0400

    pygame init

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame as py
 #hi
+py.init()
+screen = py.display.set_mode((1280, 720))
+clock = py.time.Clock()
+running = True
+
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+    py.display.flip()
+    clock.tick(60)
+py.quit()
