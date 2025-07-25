4eb797cbab1e3ae3a18bfa9a86bb05ea8280a387
11476 <gjw3503@gmail.com>
2025-07-25 19:57:29 -0400

    rects

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ while running:
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
-    screen.fill("purple")
-
+    screen.fill("beige")
+    # draw 5x5 square of rectangles
+    for i in range(5):
+        for j in range(5):
+            py.draw.rect(screen, "black", (i*200, j*200, 200, 200)) 
     py.display.flip()
     clock.tick(31)
 py.quit()
