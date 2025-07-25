ae54250059dce5789016c670b2c988774331d9e8
11476 <gjw3503@gmail.com>
2025-07-25 08:48:05 -0400

    stuff

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame as py
 #hi
 py.init()
-screen = py.display.set_mode((1280, 720))
+
 clock = py.time.Clock()
 running = True
-
+height = 720
+width = 1280
+screen = py.display.set_mode((width, height))
 while running:
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
+    screen.fill("purple")
 
     py.display.flip()
     clock.tick(31)
 py.quit()
+print("done")
