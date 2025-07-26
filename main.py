e8a768287b114139d99dc9d7a8851d68dfd52150
11476 <gjw3503@gmail.com>
2025-07-26 08:21:32 -0400

    5_rounding

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ while running:
     # center at the center of the screen, not top-left of grid
     grid_width = 5 * 100 + 4 * 20  
     grid_height = 5 * 100 + 4 * 20
-    cx = (width - grid_width) // 2
-    cy = (height - grid_height) // 2
+    cx = (width - grid_width) // 2+gap
+    cy = (height - grid_height) // 2+gap
     #draw square underlay
-    py.draw.rect(screen, "#96a0b0", (cx, cy, grid_width, grid_height))
+    py.draw.rect(screen, "#96a0b0", (cx, cy, grid_width, grid_height), border_radius=15)
     # draw 5x5 square of rectangles
    
     for i in range(5):
         for j in range(5):
-            py.draw.rect(screen, "#798394", (i*gap+cx, j*gap+cy, square_side, square_side)) 
+            py.draw.rect(screen, "#798394", (i*gap+cx, j*gap+cy, square_side, square_side), border_top_left_radius=20) 
     py.display.flip()
     py.display.set_caption(str(clock.get_fps()))
     clock.tick(60)
